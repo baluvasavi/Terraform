bf67cf97236b5c9d2b4434f73aa9aa8d3cdd0707
root <root@ip-10-1-1-106.ec2.internal>
2021-10-08 05:24:30 +0000

    add vpc code in second commit

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+	Owner = "Sree"
+    }
+}
